862a08cc94c22fa66add656c428dfa5f970681e2
Obeke Godfrey Vicent <obekegodfrey2@gmail.com>
2026-04-22 17:41:38 +0300

    project 3 & 4

---
+++ project3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+################################################################################################
+# Name: Obeke Godfrey Vicent
+################################################################################################
+echo "This program get first 10 biggest files in the file system passed via positional argument"
+path="$1"
+echo $path
+du -ah $path | sort -hr | head -n 5 > /tmp/filesize.txt
+echo "This is the list of big files in the file system $path"
+cat /tmp/filesize.txt
